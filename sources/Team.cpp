6854b2b12b7fa5566acf590c3a5ed149204c7659
malaklinux <malak.qeedan@hotmail.com>
2023-05-16 22:18:13 +0300

    Updated Team

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -9,18 +9,13 @@ namespace ariel {
     Team::~Team() {}
 
     void Team::add(Character *fighter) {
-        if (fighters.size() == 10) {
-            throw invalid_argument("ERROR add(Character* fighter) :: Team is full.\n");
-        } else {
-            fighters[fighters.size() - 1] = fighter;
-        }
     }
 
     void Team::attack(Team *enemyTeam) {
     }
 
     int Team::stillAlive() {
-        return (int) fighters.size();
+        return 0;
     }
 
     void Team::print() {
